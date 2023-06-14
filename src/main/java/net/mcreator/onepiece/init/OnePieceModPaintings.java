71f97d7a6b23f3255853c9ca7aabab5252c346ad
Oli0123 <OLLY_@FavouriteChildsPc.Home>
2023-06-14 19:40:53 +0100

    Added Boob painting.

---
+++ src/main/java/net/mcreator/onepiece/init/OnePieceModPaintings.java
@@ -0,0 +1,18 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.onepiece.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.entity.decoration.PaintingVariant;
+
+import net.mcreator.onepiece.OnePieceMod;
+
+public class OnePieceModPaintings {
+	public static final DeferredRegister<PaintingVariant> REGISTRY = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, OnePieceMod.MODID);
+	public static final RegistryObject<PaintingVariant> BOOBA = REGISTRY.register("booba", () -> new PaintingVariant(16, 32));
+}
